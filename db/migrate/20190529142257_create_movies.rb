2c1be028e0554ba6fc0901dea4faa96dad3f388a
Ethan Sagin <ethansagin@gmail.com>
2019-05-29 14:26:51 +0000

    Automatically backed up by Learn

--- db/migrate/20190529142257_create_movies.rb
+++ db/migrate/20190529142257_create_movies.rb
@@ -1,7 +1,11 @@
 class CreateMovies < ActiveRecord::Migration[5.2]
   def change
     CREATE TABLE :movies do |t|
-      t.title 
+      t.string :title 
+      t.integer :release_date 
+      t.string :director
+      t.string :lead 
+      t.boolean :in_theaters
     end
   end
 end
