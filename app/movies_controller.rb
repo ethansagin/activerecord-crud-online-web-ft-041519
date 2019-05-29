7e6a126631aab22470803614bec8311dfeb9ed7e
Ethan Sagin <ethansagin@gmail.com>
2019-05-29 14:48:55 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -88,7 +88,7 @@ end
 
 def can_destroy_a_single_item
   Movie.create(title: "That One Where the Guy Kicks Another Guy Once")
-  __
+  Movie.delete
   __
 end
 
@@ -96,5 +96,5 @@ def can_destroy_all_items_at_once
   10.times do |i|
     Movie.create(title: "Movie_#{i}")
   end
-  Movie.destroy
+  Movie.delete_all
 end
