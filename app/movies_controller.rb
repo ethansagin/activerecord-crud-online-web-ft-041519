4ed003b2cffa09e21b2b63917eb5890dcb2f90d6
Ethan Sagin <ethansagin@gmail.com>
2019-05-29 15:46:55 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -32,7 +32,7 @@ def can_be_created_in_a_block(args = {:title => "Home Alone", :release_date => 1
   Movie.create do |m|
     m.title = args[:title]
     binding.pry
-    m.release_date = :release_date
+    m.release_date = args[:release_date]
   end
 end
 
